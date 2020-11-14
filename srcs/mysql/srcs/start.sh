a9d5734e9ac66a2441487c73a90db33e933ae960
ViktorVlasov <vlasov_viktor01@mail.ru>
2020-11-14 22:47:37 +0300

    influxdb, grafana, mysql, pma

--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -2,9 +2,9 @@
 
 openrc default
 /etc/init.d/mariadb setup
-rc-service mariadb start
 
-mysql -u root < /etc/setup_request.sql
+rc-service mariadb start
+mysql -u root < /etc/create_db.sql
 #mysql wordpress -u root < /var/wordpress.sql
 
 rc-service mariadb stop
